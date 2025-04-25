31d0d4ee8d8a1ecb3646a511da9e99e805997a81
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2025-04-25 13:36:34 +0300

    Initial project structure with layered architecture and secure .gitignore configuration

---
+++ src/test/java/com/alirizakaygusuz/BorekStore/BorekStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alirizakaygusuz.BorekStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorekStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
