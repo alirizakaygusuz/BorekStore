429346b0037b0857fa3d4541ecf5ca3138310410
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2025-04-29 17:22:52 +0300

    chore(controller): Updated update endpoint annotations from @PostMapping to @PutMapping
    - Replaced @PostMapping with @PutMapping for update operations in RestCustomerController, RestStoreController, RestAddressController, and RestAccountController.

--- src/main/java/com/alirizakaygusuz/controller/impl/RestCustomerController.java
+++ src/main/java/com/alirizakaygusuz/controller/impl/RestCustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,7 +49,7 @@ public class RestCustomerController extends RestBaseController implements IRestC
 		return ok(customerService.getAllCustomers());
 	}
 
-	@PostMapping("/update/{id}")
+	@PutMapping("/update/{id}")
 	@Override
 	public RootEntity<DtoCustomer> updateCustomer(@Valid @RequestBody DtoCustomerIU dtoCustomerIU,@PathVariable Long id) {
 
